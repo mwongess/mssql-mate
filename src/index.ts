6a9a1004e2c9d362ffe207e5c26e7e199c392269
mwongess <amosmwongelah@gmail.com>
2023-08-14 13:55:54 +0300

    export type config and class connection

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,46 @@
-export default class Connection {
-    constructor(){}
+import mssql from "mssql";
+
+export interface Config {
+  user: string;
+  password: string;
+  server: string;
+  database: string;
+  options: {
+    encrypt: boolean;
+    trustServerCertificate: boolean;
+  };
+}
+export class Connection {
+  private config: Config;
+  private pool: Promise<mssql.ConnectionPool>;
+
+  constructor(config: Config) {
+    this.config = config;
+    this.pool = this.getConnection();
+  }
+
+  getConnection(): Promise<mssql.ConnectionPool> {
+    const pool = mssql.connect(this.config);
+    return pool;
+  }
+
+  createRequest(request: mssql.Request, params: { [x: string]: string }) {
+    for (const key in params) {
+      request.input(key, params[key]);
+    }
+    return request;
+  }
+
+  async executeProc(procName: string, params?: { [x: string]: string }) {
+    let pool = await this.pool;
+    let request = pool.request();
+    params ? (request = this.createRequest(request, params)) : request;
+    return await request.execute(procName);
+  }
+
+  async executeQuery(query: string, params: { [x: string]: string }) {
+    let request = (await this.pool).request();
+    params ? (request = this.createRequest(request, params)) : request;
+    return await request.query(query);
+  }
 }
