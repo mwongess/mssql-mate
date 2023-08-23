33ea5202c0df41b9b5003af24ed2e0f5afb4230a
mwongess <amosmwongelah@gmail.com>
2023-08-23 15:08:28 +0300

    create workflow to publish package

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+"use strict";
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+    moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+};
